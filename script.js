546aa2b382eddc418d14dbba5317d77be2698d35
Nasser <nasser11.software@gmail.com>
2026-08-26 10:04:07 +0300

    create function for basic path operators and operate() that takes an operator and two numbers and calls the proper function on the numbers

--- script.js
+++ script.js
@@ -7,3 +7,48 @@ modeToggle.addEventListener('change', () => {
         document.body.classList.remove('dark-mode');
     }
 })
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        console.log("Cannot divide by zero");
+    }
+    return a / b;
+}
+
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            add(a, b);
+            break;
+
+        case "-":
+            subtract(a, b);
+            break;
+
+        case "*":
+            multiply(a, b);
+            break;
+
+        case "/":
+            divide(a, b);
+            break;
+
+        default:
+            console.log("Wrong operator");
+    }
+}
